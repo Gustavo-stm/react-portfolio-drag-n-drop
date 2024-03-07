88e5305ad06cc360c31c2db5ed221c8e2ceae633
Gustavo-stm <gustavo-catala-sverdrup@sanktamaria.fhsk.se>
2024-03-07 15:05:28 +0100

    Adding image modules config to get rid of typescript errors when importing image on react

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 })
